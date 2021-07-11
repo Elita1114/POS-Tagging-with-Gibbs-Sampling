0288fdb08ce502ddcd879bf33f4ad8625dba061c
Elita1114 <58215433+Elita1114@users.noreply.github.com>
2021-07-11 13:26:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,32 +4,40 @@ from collections import defaultdict, Counter
 from file_utils import *
 
 
+def normalize_vector(vetor):
+    normalize_vector = Counter()
+    sum_word = sum(vetor.values())
+    for tag in vetor.keys():
+        normalize_vector[tag] = vetor[tag] / sum_word
+    return normalize_vector
 
-def build_emmission_transition_prob(emission_counter):
+
+def build_emmission_prob(emission_counter):
     emission_prob = defaultdict(Counter)
-
     for word in emission_counter.keys():
-        sum_word = sum(emission_counter[word].values())
-        for tag in emission_counter[word].keys():
-            emission_prob[word][tag] = emission_counter[word][tag] / sum_word
+        emission_prob[word] = normalize_vector(emission_counter[word])
     return emission_prob
 
 
 def build_emmision(emission_counter, corpus_words, corpus_tags):
     for word, tag in zip(corpus_words, corpus_tags):
         emission_counter[word][tag] += 1
-    emission_prob = build_emmission_transition_prob(emission_counter)
+    emission_prob = build_emmission_prob(emission_counter)
     return emission_prob
 
 
-
+def build_transition(corpus_tags, transition_counter, transition_length):
+    for idx in range(transition_length, len(corpus_tags)):
+        transition_counter[tuple(corpus_tags[idx-transition_length:idx+1])]+=1
+    transition_prob = normalize_vector(transition_counter)
+    return transition_prob
 
 def main():
     corpus_words, corpus_tags, indeces_untagged_words, lemmas = arrange_corpus(train_path, dev_path, test_path, PADDING_START_END_length, pos_tags_learning)
     emission_counter = defaultdict(Counter)
-    transition = defaultdict(Counter)
+    transition_counter = Counter()
     emission = build_emmision(emission_counter, corpus_words, corpus_tags)
-
+    transition = build_transition(corpus_tags, transition_counter, transition_length)
 
     i =1
 
@@ -57,6 +65,7 @@ if __name__ == '__main__':
     dev_path = args.dev_path
     test_path = args.test_path
     PADDING_START_END_length = args.PADDING_START_END_length
+    transition_length = args.transition_length
 
     pos_tags_learning = [".", ","]
 
